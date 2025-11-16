8ff04e49da9e4d45055bb979c2a7a3cb287f954f
Mathieu-Boyer <mboyer980@gmail.com>
2025-11-16 15:00:37 +0100

    Triangle appearing on screen

--- include/GLApp.hpp
+++ include/GLApp.hpp
@@ -30,6 +30,25 @@ GLApp::GLApp(const int width, const int height,  const std::string &name) : _wid
 
 }
 
+float vertices[] = {
+    -0.75,-0.75,0.0,
+    0,0.75,0.0,
+    0.75,-0.75,0.0
+};
+
+const char *vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main (){"
+    "gl_Position = vec4(aPos, 1);"
+"}\0";
+
+const char *fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 fragColor;\n"
+"void main (){"
+    "fragColor = vec4(1,1, 1, 1);"
+"}\0";
 
 void GLApp::init(){
     glfwInit();
@@ -47,10 +66,42 @@ void GLApp::init(){
 
 void GLApp::render()
 {
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VBO, VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3 , GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)0);
+    glEnableVertexAttribArray(0);
+
+
     while (!glfwWindowShouldClose(_window)){
         glfwPollEvents();
         glClearColor(.2, .4, .6, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(_window);
     }
 }
